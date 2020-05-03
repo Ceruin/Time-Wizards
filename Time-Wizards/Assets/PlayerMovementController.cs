57e4951ae3aa6af02442f1b0edc421025b34e628
Ceruin <13532471+Ceruin@users.noreply.github.com>
2020-05-03 01:28:04 -0400

    Death by a thousand poops

---
+++ Time-Wizards/Assets/PlayerMovementController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 10;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(new Vector3(horizontalInput, 0, verticalInput) * moveSpeed * Time.deltaTime);
+
+    }
+}
---
+++ Time-Wizards/Assets/TimerController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Timers;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerController : MonoBehaviour
+{ 
+    [SerializeField] float totalTime = 300f; // 5 Minutes
+
+    Text countdownTimer;
+
+    // timer needs to go down and up
+    // timer needs a base time to go down from
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        countdownTimer = GetComponent<Text>();
+        StartCoroutine(Countdown());
+        for (int i = 0; i < 1000000000; i++) {
+            print("poop");
+        }
+    }
+
+    IEnumerator Countdown()
+    {        
+        while (totalTime >= 0)
+        {
+            totalTime--;
+            countdownTimer.text = "Time: " + totalTime;
+            yield return new WaitForSeconds(1);
+        }
+    }
+
+    int GetMinutes(int totalTime)
+    {
+
+        return 0;
+    }
+}
