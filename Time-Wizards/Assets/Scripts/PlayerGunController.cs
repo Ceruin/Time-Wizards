39ce5f46cf03a13f16dfad69f72c6aa9f0ad41c5
Ceruin <13532471+Ceruin@users.noreply.github.com>
2020-05-07 00:36:55 -0400

    Cannon

---
+++ Time-Wizards/Assets/Scripts/PlayerGunController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGunController : MonoBehaviour
+{
+    [SerializeField] GameObject cannonBall;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire"))
+        {
+            GameObject newBall = Instantiate(cannonBall, transform.position, Quaternion.identity);
+            Rigidbody test = newBall.gameObject.GetComponentInChildren<Rigidbody>();
+
+            test.AddExplosionForce(1000, Vector3.up, 100);
+        }
+    }
+}
---
+++ Time-Wizards/Assets/Scripts/PlayerMovementController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour
+{   
+    CharacterController characterController;
+
+    [SerializeField] float moveSpeed = 6.0f;
+    [SerializeField] float jumpSpeed = 8.0f;
+    [SerializeField] float gravity = 20.0f;
+
+    private Vector3 moveDirection = Vector3.zero;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        if (characterController.isGrounded)
+        {
+            // We are grounded, so recalculate
+            // move direction directly from axes
+
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f,Input.GetAxis("Vertical"));      
+            moveDirection *= moveSpeed;
+
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+
+        // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
+        // when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
+        // as an acceleration (ms^-2)
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        // Move the controller
+        characterController.Move(moveDirection * Time.deltaTime);
+    }
+}
